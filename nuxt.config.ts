391a7ce6f169d334c22d6ad35e4016de0bebf16b
nathan <cornelie.nathanpro@gmail.com>
2024-10-14 19:39:56 +0200

    edit workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+          additionalData: '@forward "~/assets/scss/_variables.scss" as *;',
         },
       },
     },
