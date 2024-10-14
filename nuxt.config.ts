ffb501b998c5aa318d151d715fcdb0fa7c975bf8
nathan <cornelie.nathanpro@gmail.com>
2024-10-14 20:03:46 +0200

    css modifications

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  ssr: false, // Désactiver SSR pour un site statique
+  // target: 'static', // Permet de générer un site statique
+  app: {
+    baseURL: process.env.BASE_URL || "/", // Chemin de base (important pour GitHub Pages)
+  },
+  vite: {
+    base: process.env.BASE_URL || "/",
+  },
   css: ["@mdi/font/css/materialdesignicons.css"],
   modules: ["vuetify-nuxt-module", "nuxt-mdi", "@nuxt/image"],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-         
-        },
-      },
-    },
-  },
+
   vuetify: {
     vuetifyOptions: {
       icons: {
